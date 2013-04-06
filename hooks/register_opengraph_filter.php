d08b719b05fc66303311cb676e956eabfbd6e9c7
Abbas Adel <abbas.adel@mufix.org>
2013-04-06 13:55:19 +0200

    add homepage metatags

--- hooks/register_opengraph_filter.php
+++ hooks/register_opengraph_filter.php
@@ -13,50 +13,55 @@ class OpenGraph {
     }
 
     public function add() {
+
+        $data = Event::$data;
+        $htmlutil = new Htmlutil();
+        $html = $htmlutil->parse($data, true, false);
+
+        $title = $html->find('title ', 0)->innertext;
+        $meta = array();
+
+        if (Kohana::config('settings.site_banner_id') != NULL) {
+            $banner = ORM::factory('media')->find(Kohana::config('settings.site_banner_id'));
+            $logo = url::convert_uploaded_to_abs($banner->media_link);
+            $meta['og:image'][] = $logo;
+        }
+
+        $meta['og:title'] = $title;
+
+
         if (Router::$controller == 'reports' && Router::$method == 'view') {
 
-            $data = Event::$data;
-            $htmlutil = new Htmlutil();
-            $html = $htmlutil->parse($data, true, false);
+            $meta['og:description'] = @$html->find('.report-description-text', 0)->plaintext;
 
-            $meta = array();
-
-            $title = $html->find('title ', 0)->innertext;
-            $desc = @$html->find('.report-description-text', 0)->plaintext;
             $photos = @$html->find('.photothumb');
-            $logo = @$html->find('#page img', 1)->src;
-
-
-
-            $meta['og:title'] = $title;
-            $meta['og:description'] = $desc;
-
 
             foreach ($photos as $photo) {
                 $meta['og:image'][] = $photo->href;
             }
-
-            $meta['og:image'][] = $logo;
-
-
-            $meta_str = '';
-            foreach ($meta as $key => $val) {
-                if (is_array($val)) {
-                    foreach ($val as $img) {
-                        $meta_str .= '<meta property="' . $key . '" content="' . $img . '"/>';
-                    }
-                    continue;
-                }
-                $meta_str .= '<meta property = "' . $key . '" content = "' . $val . '"/>
-                    ';
-            }
-
-            $html->find('head', 0)->innertext .= $meta_str;
-
-
-
-            Event::$data = $html;
         }
+
+
+        if (!isset($meta['og:description'])) {
+            $meta['og:description'] = Kohana::config('settings.site_tagline');
+        }
+
+
+        $meta_str = '';
+        foreach ($meta as $key => $val) {
+            if (is_array($val)) {
+                foreach ($val as $img) {
+                    $meta_str .= '<meta property="' . $key . '" content="' . $img . '"/>';
+                }
+                continue;
+            }
+            $meta_str .= '<meta property = "' . $key . '" content = "' . $val . '"/>
+                    ';
+        }
+
+
+        $html->find('head', 0)->innertext .= $meta_str;
+        Event::$data = $html;
     }
 
 }
